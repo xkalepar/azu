f5b57b33a88752a0f3dc5bfa69d218a05b75be53
LastHolmz <155854955+LastHolmz@users.noreply.github.com>
2024-07-24 03:07:30 +0200

    collage / magazines compeletely done

--- app/[lang]/collages/[collage]/magazines/[magazine]/page.tsx
+++ app/[lang]/collages/[collage]/magazines/[magazine]/page.tsx
@@ -1,9 +1,197 @@
-import React from 'react'
+import ParseData from "@/app/components/parse-data";
+import { Skeleton } from "@/components/ui/skeleton";
+import { notFound } from "next/navigation";
+import { Metadata } from "next/types";
+import { Suspense } from "react";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import Link from "next/link";
 
-const page = () => {
-    return (
-        <div>page</div>
-    )
+import { getMagazine } from "@/app/dashboard/university/magazines/seed";
+import { getAllMagazines } from "@/prisma/seed";
+import { DownloadCloudIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+import Lang from "@/app/[lang]/components/lang";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { magazine: string; lang: "ar" | "en" };
+}): Promise<Metadata> {
+  const { magazine: magazineId, lang } = params;
+  const magazine = await getMagazine({ id: magazineId });
+  if (!magazine) {
+    return {
+      title: "404 غير موجود",
+    };
+  }
+
+  return {
+    title:
+      lang === "ar"
+        ? ` المجلات | ${magazine.arContent?.title}`
+        : ` magazines | ${magazine.enContent?.title}`,
+    description:
+      lang === "ar" ? magazine.arContent?.body : magazine.enContent?.body,
+  };
 }
 
-export default page
+export async function generateStaticParams(
+
+) {
+  const magazine = await getAllMagazines({});
+  return magazine.map((id) => ({ id: id.id }));
+}
+
+const magazinePage = async ({
+  params,
+}: {
+  params: { magazine: string; lang: "ar" | "en" };
+}) => {
+  const { magazine: id } = params;
+  const magazine = await getMagazine({ id });
+  console.log(magazine)
+  if (!magazine) {
+    return notFound();
+  }
+  const { lang } = params;
+  return (
+    <main className="container xl:mx-4 xl:px-4">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href={`/${lang}`}>
+                <Lang lang={lang} ar={"الرئيسية"} en={"home"} />
+              </Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href={`/${lang}/magazines`}>
+                <Lang lang={lang} ar={"المجلة"} en={"magazine"} />
+              </Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>
+              <Lang
+                ar={magazine.arContent?.title}
+                en={magazine.enContent?.title}
+                lang={lang}
+              />
+            </BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
+      <Suspense
+        fallback={
+          <>
+            <Skeleton className=" min-w-full max-w-full min-h-full"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+            <Skeleton className=" min-w-full h-1 my-1"></Skeleton>
+          </>
+        }
+      >
+        <Lang
+          lang={lang}
+          ar={
+            <ParseData dir="rtl" content={magazine.arContent?.body ?? ""} />
+          }
+          en={
+            <ParseData dir="ltr" content={magazine.enContent?.body ?? ""} />
+          }
+        />
+
+        <Link className={cn(" w-full my-2 sm:w-fit mx-auto flex justify-center items-center gap-2", "bg-primary text-primary-foreground shadow hover:bg-primary/90 rounded-md px-4 py-2")} target={"_blank"}
+          download={true}
+          href={magazine!.pdfUri as string}>
+          <DownloadCloudIcon></DownloadCloudIcon>
+          <Lang
+            lang={lang}
+            ar={
+              "تحميل"
+            }
+            en={
+              "download"
+            }
+          />
+        </Link>
+
+      </Suspense>
+    </main>
+  );
+};
+
+export default magazinePage;
--- app/[lang]/collages/[collage]/magazines/page.tsx
+++ app/[lang]/collages/[collage]/magazines/page.tsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import CardPreview from "@/app/components/card-preview";
+import { getMagazines } from "@/prisma/seed";
+import React from "react";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import Link from "next/link";
+import { cutString } from "@/lib/utils";
+import ParseData from "@/app/components/parse-data";
+import { Metadata } from "next";
+import { env } from "process";
+import Lang from "@/app/[lang]/components/lang";
+import { getDir } from "@/app/[lang]/components/footers/home-footer";
+type Lang = "en" | "ar";
+export const metadata: Metadata = {
+  title: "المجلات العلمية",
+};
+const page = async ({ params }: { params: { lang: Lang, collage: string } }) => {
+  const { lang } = params;
+  const magazines = await getMagazines({
+    page: 1,
+    qty: 20,
+    linkedId: params.collage
+  });
+  return (
+    <main className=" container ">
+      <Breadcrumb dir="rtl">
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href={`/${lang}`}>
+                <Lang lang={lang} ar={"الرئيسية"} en={"home"} />
+              </Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>
+              <Lang lang={lang} ar={"المجلة"} en={"magazine"} />
+            </BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      <div className="grid md:grid-cols-2 my-2 sm:grid-cols-3 gap-2 lg:grid-cols-4">
+        {magazines.map((item, index) => {
+          return (
+            <CardPreview
+              key={index}
+              lang={lang}
+              className=" h-[400px]"
+              src={item.logo}
+              alt={
+                lang === "ar" ? item.arContent?.title : item.enContent?.title
+              }
+              title={
+                lang === "ar" ? item.arContent?.title : item.enContent?.title
+              }
+              href={`/${lang}/magazines/${item.id}`}
+            >
+              <div className="my-1 py-1" dir={getDir(lang)}>
+                <Lang
+                  ar={
+                    <ParseData
+                      dir="rtl"
+                      content={cutString(item.arContent?.body ?? "", 100)}
+                    />
+                  }
+                  en={
+                    <ParseData
+                      dir="ltr"
+                      content={cutString(item.enContent?.body ?? "", 100)}
+                    />
+                  }
+                  lang={lang}
+                />
+              </div>
+            </CardPreview>
+          );
+        })}
+      </div>
+    </main>
+  );
+};
 
-const page = () => {
-    return (
-        <div>page</div>
-    )
-}
-
-export default page
+export default page;
