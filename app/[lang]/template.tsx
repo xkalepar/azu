e26da0923ff8a7f0439456da4c7a2cd4fd33c57d
hodifa <databaserepos@gmail.com>
2024-10-08 17:05:05 +0200

    domain

--- app/[lang]/template.tsx
+++ app/[lang]/template.tsx
@@ -1,23 +1,15 @@
 "use client";
 import { motion } from "framer-motion";
-// import Loading from "./components/loading";
-// import { Suspense } from "react";
 
 const Template = ({ children }: { children: React.ReactNode }) => {
-  // const { scrollYProgress } = useScroll();
-
   return (
     <motion.div
       initial={{ y: 20, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ ease: "easeInOut", duration: 1 }}
-      // style={{ scaleX: scrollYProgress }}
     >
       {children}
     </motion.div>
-    // <Suspense fallback={<div className=" flex-center h-full w-full"></div>}>
-    //   {children}
-    // </Suspense>
   );
 };
 
